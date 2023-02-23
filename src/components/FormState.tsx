7db5ccf84d1f51b58598999f077b9b5809ffedcf
guigattidev <gattiwhoomps@hotmail.com>
2023-02-23 14:57:15 -0300

    feat: set a current value with button

--- src/components/FormState.tsx
+++ src/components/FormState.tsx
@@ -5,76 +5,37 @@ let renderCount = 0;
 
 interface FormInputs {
   firstName: string;
-  lastName: string;
 }
 
 export function FormState() {
   // Boolean value that indicates whether form changes
-  const {
-    register,
-    handleSubmit,
-    formState,
-    formState: { errors },
-  } = useForm<FormInputs>({
-    mode: "onChange",
+  const { register, setValue, handleSubmit } = useForm<FormInputs>({
     defaultValues: {
       firstName: "",
-      lastName: "",
     },
   });
   renderCount++;
-
-  const onSubmit = async (data) => {
-    await sleep(3000);
-  };
-
-  // Check when formState was changed
-  React.useEffect(() => {
-    console.log("useEffect", formState.errors);
-  }, [formState]);
-
-  // Check if is valid on run time
-  // console.log("isValidating", isValidating);
-
-  // Check if is valid on run time
-  // console.log("isSubmitting", isSubmitting);
-
-  // Check if is valid on run time
-  // console.log("isvalid", isValid);
-
-  // Check how many times submit was submitted
-  // console.log("submitCount", submitCount);
-
-  // Check if submited is successfully
-  // console.log("isSubmitSuccessful", isSubmitSuccessful);
-
-  // Indicates a input get focus and blur
-  // console.log("touchedFields", touchedFields);
-
-  // Check if field was dirty, need provide default values
-  // console.log("DirtyFields", dirtyFields);
-
-  // Where of was form dirty
-  // console.log("IsDirty", isDirty);
-
-  // Check for errors on fields
-  // console.log("Errors", errors);
 
   return (
     <div>
       <form
         onSubmit={handleSubmit((data) => {
-          console.log(data);
+          console.log("submitted", data);
         })}
       >
         <input
           {...(register("firstName"), { required: true })}
           placeholder="First Name"
         />
-        <input
-          {...(register("lastName"), { required: true })}
-          placeholder="Last Name"
-        />
+
+        <button
+          type="button"
+          onClick={() => {
+            setValue("firstName", "bill");
+          }}
+        >
+          setValue
+        </button>
 
         <input type="submit" />
       </form>
