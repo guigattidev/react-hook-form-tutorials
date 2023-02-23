41f1148bc3b392714060e7fc9caf39fa73555615
guigattidev <gattiwhoomps@hotmail.com>
2023-02-23 15:29:08 -0300

    feat: setvalue component

--- src/components/FormState.tsx
+++ src/components/FormState.tsx
@@ -5,53 +5,76 @@ let renderCount = 0;
 
 interface FormInputs {
   firstName: string;
+  lastName: string;
 }
 
-export function FormState() {
+export function App() {
   // Boolean value that indicates whether form changes
   const {
     register,
     handleSubmit,
-    setValue,
+    formState,
     formState: { errors },
   } = useForm<FormInputs>({
+    mode: "onChange",
     defaultValues: {
-      yourDetails: {
-        firstName: "",
-        lastName: "",
-      },
+      firstName: "",
+      lastName: "",
     },
   });
   renderCount++;
 
-  console.log("errors", errors);
+  const onSubmit = async (data) => {
+    await sleep(3000);
+  };
+
+  // Check when formState was changed
+  React.useEffect(() => {
+    console.log("useEffect", formState.errors);
+  }, [formState]);
+
+  // Check if is valid on run time
+  // console.log("isValidating", isValidating);
+
+  // Check if is valid on run time
+  // console.log("isSubmitting", isSubmitting);
+
+  // Check if is valid on run time
+  // console.log("isvalid", isValid);
+
+  // Check how many times submit was submitted
+  // console.log("submitCount", submitCount);
+
+  // Check if submited is successfully
+  // console.log("isSubmitSuccessful", isSubmitSuccessful);
+
+  // Indicates a input get focus and blur
+  // console.log("touchedFields", touchedFields);
+
+  // Check if field was dirty, need provide default values
+  // console.log("DirtyFields", dirtyFields);
+
+  // Where of was form dirty
+  // console.log("IsDirty", isDirty);
+
+  // Check for errors on fields
+  // console.log("Errors", errors);
 
   return (
     <div>
       <form
         onSubmit={handleSubmit((data) => {
-          console.log("submitted", data);
+          console.log(data);
         })}
       >
         <input
-          {...(register("youDetails.firstName"), { required: true })}
+          {...(register("firstName"), { required: true })}
           placeholder="First Name"
         />
-
         <input
-          {...(register("youDetails.lastName"), { required: true })}
+          {...(register("lastName"), { required: true })}
           placeholder="Last Name"
         />
-
-        <button
-          type="button"
-          onClick={() => {
-            setValue("youDetails.firstName", "Bill");
-            setValue("youDetails.lastName", "Lee");
-          }}
-        >
-          setValue
-        </button>
 
         <input type="submit" />
       </form>
---
+++ src/components/SetValue.tsx
@@ -0,0 +1,60 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
+
+let renderCount = 0;
+
+interface FormInputs {
+  firstName: string;
+}
+
+export function SetValue() {
+  // Boolean value that indicates whether form changes
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<FormInputs>({
+    defaultValues: {
+      yourDetails: {
+        firstName: "",
+        lastName: "",
+      },
+    },
+  });
+  renderCount++;
+
+  console.log("errors", errors);
+
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit((data) => {
+          console.log("submitted", data);
+        })}
+      >
+        <input
+          {...(register("youDetails.firstName"), { required: true })}
+          placeholder="First Name"
+        />
+
+        <input
+          {...(register("youDetails.lastName"), { required: true })}
+          placeholder="Last Name"
+        />
+
+        <button
+          type="button"
+          onClick={() => {
+            setValue("youDetails.firstName", "Bill");
+            setValue("youDetails.lastName", "Lee");
+          }}
+        >
+          setValue
+        </button>
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
